c91a820f57d5db050078d74b11b4e5aa8564a098
bodascaluca <bodasca.luca@gmail.com>
2022-05-05 14:23:27 +0200

    Scaffolding

---
+++ js/script.js
@@ -0,0 +1,10 @@
+// Vue.config.devtools = true;
+  const app = new Vue(
+      {
+      el: '#root',
+      data:{
+          title: `Ciao sono un titolo`,
+          userName:`Luca`,
+      }
+  }
+  );
