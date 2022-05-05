96fb9d1339c3082467fea977fd491c6aaaec3adb
bodascaluca <bodasca.luca@gmail.com>
2022-05-05 16:55:32 +0200

    Add h1 with data

--- js/script.js
+++ js/script.js
@@ -1,10 +1,15 @@
-// Vue.config.devtools = true;
-  const app = new Vue(
-      {
-      el: '#root',
-      data:{
-          title: `Ciao sono un titolo`,
-          userName:`Luca`,
-      }
-  }
-  );
+
+// Descrizione:
+// Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.
+// Bonus:
+// Aggiungere alla pagina un’immagine, presa anch’essa da un data.
+
+
+Vue.config.devtools = true;
+
+const app = new Vue({
+    el : `#root`,
+    data:{
+        title: `Oggi ti mando un link per aprezzare meglio la cultura di Brescia`
+    }
+});
